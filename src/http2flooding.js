b0f33cfcd300d0994d2ed6c5f85c879ecf8cedcb
Franckccv <francescocaccavale95@gmail.com>
2023-03-14 11:25:49 +0100

    Remove TLS

--- src/http2flooding.js
+++ src/http2flooding.js
@@ -32,11 +32,8 @@ class Http2Flooding {
  * 
  */
     connectToServer(){
-      let address= 'https://'.concat(this.ip).concat(':').concat(this.port).concat('/');
-      const client = http2.connect(address,{
-        requestCert: false, // put true if you want a client certificate, tested and it works
-        rejectUnauthorized: false
-        });
+      let address= 'http://'.concat(this.ip).concat(':').concat(this.port).concat('/');
+      const client = http2.connect(address);
         return client;
     }
 
